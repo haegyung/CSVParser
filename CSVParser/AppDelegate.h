151f71958de818726a011c4d8a481e3afbf4a789
Anton Terekhov <terekhov.anton@gmail.com>
2014-03-08 10:11:39 +0400

    Initial Commit

---
+++ CSVParser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CSVParser
+//
+//  Created by Anton Terekhov on 08/03/14.
+//  Copyright (c) 2014 Anton Terekhov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CSVParser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CSVParser
+//
+//  Created by Anton Terekhov on 08/03/14.
+//  Copyright (c) 2014 Anton Terekhov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
